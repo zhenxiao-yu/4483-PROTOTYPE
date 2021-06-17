ed4c3dfbb3b607fa3e9718bfdb88409824f3dac6
Tutor <DungeonGunnerCourse>
2021-06-17 23:13:30 +0100

    Instantiate Dungeon Room Tilemaps

--- Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -1,18 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(BoxCollider2D))]
 public class InstantiatedRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public Room room;
+    [HideInInspector] public Grid grid;
+    [HideInInspector] public Tilemap groundTilemap;
+    [HideInInspector] public Tilemap decoration1Tilemap;
+    [HideInInspector] public Tilemap decoration2Tilemap;
+    [HideInInspector] public Tilemap frontTilemap;
+    [HideInInspector] public Tilemap collisionTilemap;
+    [HideInInspector] public Tilemap minimapTilemap;
+    [HideInInspector] public Bounds roomColliderBounds;
+
+    private BoxCollider2D boxCollider2D;
+
+    private void Awake()
     {
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        // Save room collider bounds
+        roomColliderBounds = boxCollider2D.bounds;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Initialise The Instantiated Room
+    /// </summary>
+    public void Initialise(GameObject roomGameobject)
     {
-        
+        PopulateTilemapMemberVariables(roomGameobject);
+
+        DisableCollisionTilemapRenderer();
+
     }
+
+    /// <summary>
+    /// Populate the tilemap and grid memeber variables.
+    /// </summary>
+    private void PopulateTilemapMemberVariables(GameObject roomGameobject)
+    {
+        // Get the grid component.
+        grid = roomGameobject.GetComponentInChildren<Grid>();
+
+        // Get tilemaps in children.
+        Tilemap[] tilemaps = roomGameobject.GetComponentsInChildren<Tilemap>();
+
+        foreach (Tilemap tilemap in tilemaps)
+        {
+            if (tilemap.gameObject.tag == "groundTilemap")
+            {
+                groundTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "decoration1Tilemap")
+            {
+                decoration1Tilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "decoration2Tilemap")
+            {
+                decoration2Tilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "frontTilemap")
+            {
+                frontTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "collisionTilemap")
+            {
+                collisionTilemap = tilemap;
+            }
+            else if (tilemap.gameObject.tag == "minimapTilemap")
+            {
+                minimapTilemap = tilemap;
+            }
+
+        }
+
+    }
+
+    /// <summary>
+    /// Disable collision tilemap renderer
+    /// </summary>
+    private void DisableCollisionTilemapRenderer()
+    {
+        // Disable collision tilemap renderer
+        collisionTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+
+    }
+
 }

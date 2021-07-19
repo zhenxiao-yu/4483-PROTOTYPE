7710ba21ba74d5c1783eb1350ab71a9149849fcc
Tutor <DungeonGunnerCourse>
2021-07-19 22:52:49 +0100

    Collision Matrix & Animator Speed

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -35,6 +35,7 @@ public static class Settings
     public static int rollRight = Animator.StringToHash("rollRight");
     public static int rollLeft = Animator.StringToHash("rollLeft");
     public static int rollDown = Animator.StringToHash("rollDown");
+    public static float baseSpeedForPlayerAnimations = 8f;
 
     // Animator parameters - Door
     public static int open = Animator.StringToHash("open");

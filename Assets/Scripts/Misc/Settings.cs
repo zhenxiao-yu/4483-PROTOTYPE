32c427f6338c45108aaa67c8221a55dead410c6c
Tutor <DungeonGunnerCourse>
2021-11-13 22:33:05 +0000

    Ammo Damage To Enemies

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -17,7 +17,7 @@ public static class Settings
     #region ROOM SETTINGS
     public const float fadeInTime = 0.5f; // time to fade in the room
     public const int maxChildCorridors = 3; // Max number of child corridors leading from a room. - maximum should be 3 although this is not recommended since it can cause the dungeon building to fail since the rooms are more likely to not fit together;
-
+    public const float doorUnlockDelay = 1f;
     #endregion
 
 
@@ -62,6 +62,10 @@ public static class Settings
     public const float playerMoveDistanceToRebuildPath = 3f;
     public const float enemyPathRebuildCooldown = 2f;
 
+    #endregion
+
+    #region ENEMY PARAMETERS
+    public const int defaultEnemyHealth = 20;
     #endregion
 
     #region UI PARAMETERS
--- Assets/Scripts/StaticEvents/StaticEventHandler.cs
+++ Assets/Scripts/StaticEvents/StaticEventHandler.cs
@@ -12,9 +12,23 @@ public static class StaticEventHandler
     {
         OnRoomChanged?.Invoke(new RoomChangedEventArgs() { room = room });
     }
+
+
+    // Room enemies defeated event
+    public static event Action<RoomEnemiesDefeatedArgs> OnRoomEnemiesDefeated;
+
+    public static void CallRoomEnemiesDefeatedEvent(Room room)
+    {
+        OnRoomEnemiesDefeated?.Invoke(new RoomEnemiesDefeatedArgs() { room = room });
+    }
 }
 
 public class RoomChangedEventArgs : EventArgs
+{
+    public Room room;
+}
+
+public class RoomEnemiesDefeatedArgs : EventArgs
 {
     public Room room;
 }

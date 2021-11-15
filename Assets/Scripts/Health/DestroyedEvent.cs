b908e0710a843bc6b4ce9e0a0e7d391cf19d51c9
Tutor <DungeonGunnerCourse>
2021-11-15 20:34:11 +0000

    Ammo Damage To Player

--- Assets/Scripts/Health/DestroyedEvent.cs
+++ Assets/Scripts/Health/DestroyedEvent.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class DestroyedEvent : MonoBehaviour
 {
-    public event Action<DestroyedEvent> OnDestroyed;
+    public event Action<DestroyedEvent, DestroyedEventArgs> OnDestroyed;
 
-    public void CallDestroyedEvent()
+    public void CallDestroyedEvent(bool playerDied)
     {
-        OnDestroyed?.Invoke(this);
+        OnDestroyed?.Invoke(this, new DestroyedEventArgs() { playerDied = playerDied });
     }
 }
+
+public class DestroyedEventArgs : EventArgs
+{
+    public bool playerDied;
+}
+

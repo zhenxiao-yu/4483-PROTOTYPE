db524384885705f679335e7af9ff2ecee233c118
Tutor <DungeonGunnerCourse>
2021-12-01 18:18:51 +0000

    Player Scoring

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -34,6 +34,7 @@ public class GameManager : SingletonMonobehaviour<GameManager>
 
     [HideInInspector] public GameState gameState;
     [HideInInspector] public GameState previousGameState;
+    private long gameScore;
 
     protected override void Awake()
     {
@@ -67,12 +68,18 @@ public class GameManager : SingletonMonobehaviour<GameManager>
     {
         // Subscribe to room changed event.
         StaticEventHandler.OnRoomChanged += StaticEventHandler_OnRoomChanged;
+
+        // Subscribe to the points scored event
+        StaticEventHandler.OnPointsScored += StaticEventHandler_OnPointsScored;
     }
 
     private void OnDisable()
     {
         // Unsubscribe from room changed event
         StaticEventHandler.OnRoomChanged -= StaticEventHandler_OnRoomChanged;
+
+        // Unsubscribe from the points scored event
+        StaticEventHandler.OnPointsScored -= StaticEventHandler_OnPointsScored;
 
     }
 
@@ -84,12 +91,26 @@ public class GameManager : SingletonMonobehaviour<GameManager>
         SetCurrentRoom(roomChangedEventArgs.room);
     }
 
+    /// <summary>
+    /// Handle points scored event
+    /// </summary>
+    private void StaticEventHandler_OnPointsScored(PointsScoredArgs pointsScoredArgs)
+    {
+        // Increase score
+        gameScore += pointsScoredArgs.points;
+
+        // Call score changed event
+        StaticEventHandler.CallScoreChangedEvent(gameScore);
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
         previousGameState = GameState.gameStarted;
         gameState = GameState.gameStarted;
+
+        // Set score to zero
+        gameScore = 0;
     }
 
     // Update is called once per frame

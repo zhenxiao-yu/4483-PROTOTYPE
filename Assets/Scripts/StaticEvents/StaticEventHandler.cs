72c663616bf2ff0a9f569bd3b9706b14392ef97f
Tutor <DungeonGunnerCourse>
2021-12-01 22:39:55 +0000

    Player Scoring Multiplier

--- Assets/Scripts/StaticEvents/StaticEventHandler.cs
+++ Assets/Scripts/StaticEvents/StaticEventHandler.cs
@@ -33,9 +33,17 @@ public static class StaticEventHandler
     // Score changed event
     public static event Action<ScoreChangedArgs> OnScoreChanged;
 
-    public static void CallScoreChangedEvent(long score)
+    public static void CallScoreChangedEvent(long score, int multiplier)
     {
-        OnScoreChanged?.Invoke(new ScoreChangedArgs() { score = score });
+        OnScoreChanged?.Invoke(new ScoreChangedArgs() { score = score, multiplier = multiplier });
+    }
+
+    // Multiplier event
+    public static event Action<MultiplierArgs> OnMultiplier;
+
+    public static void CallMultiplierEvent(bool multiplier)
+    {
+        OnMultiplier?.Invoke(new MultiplierArgs() { multiplier = multiplier });
     }
 }
 
@@ -57,5 +65,11 @@ public class PointsScoredArgs : EventArgs
 public class ScoreChangedArgs : EventArgs
 {
     public long score;
+    public int multiplier;
 }
 
+public class MultiplierArgs : EventArgs
+{
+    public bool multiplier;
+
+}

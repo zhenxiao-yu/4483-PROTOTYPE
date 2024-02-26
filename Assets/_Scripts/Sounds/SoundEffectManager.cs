72fd00bf03b180550c97aa60068c0d275dfd6fb4
Mark Yu <zyu347@uwo.ca>
2024-02-26 17:07:03 -0500

    changed paused button

--- Assets/_Scripts/Sounds/SoundEffectManager.cs
+++ Assets/_Scripts/Sounds/SoundEffectManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class SoundEffectManager : SingletonMonobehaviour<SoundEffectManager>
 {
-    public int soundsVolume = 8;
+    public int soundsVolume = 5;
 
     private void Start()
     {
@@ -12,22 +12,18 @@ public class SoundEffectManager : SingletonMonobehaviour<SoundEffectManager>
         {
             soundsVolume = PlayerPrefs.GetInt("soundsVolume");
         }
-       
+
         SetSoundsVolume(soundsVolume);
     }
 
     private void OnDisable()
     {
-        // Save volume settings in playerprefs
         PlayerPrefs.SetInt("soundsVolume", soundsVolume);
     }
 
-    /// <summary>
-    /// Play the sound effect
-    /// </summary>
+
     public void PlaySoundEffect(SoundEffectSO soundEffect)
     {
-        // Play sound using a sound gameobject and component from the object pool
         SoundEffect sound = (SoundEffect)PoolManager.Instance.ReuseComponent(soundEffect.soundPrefab, Vector3.zero, Quaternion.identity);
         sound.SetSound(soundEffect);
         sound.gameObject.SetActive(true);
@@ -36,49 +32,33 @@ public class SoundEffectManager : SingletonMonobehaviour<SoundEffectManager>
     }
 
 
-    /// <summary>
-    /// Disable sound effect object after it has played thus returning it to the object pool
-    /// </summary>
     private IEnumerator DisableSound(SoundEffect sound, float soundDuration)
     {
         yield return new WaitForSeconds(soundDuration);
         sound.gameObject.SetActive(false);
     }
 
-    //
-    /// <summary>
-    /// Increase sounds volume
-    /// </summary>
+
     public void IncreaseSoundsVolume()
     {
         int maxSoundsVolume = 20;
-
         if (soundsVolume >= maxSoundsVolume) return;
-
         soundsVolume += 1;
-
         SetSoundsVolume(soundsVolume); ;
     }
 
-    /// <summary>
-    /// Decrease sounds volume
-    /// </summary>
+
     public void DecreaseSoundsVolume()
     {
         if (soundsVolume == 0) return;
-
         soundsVolume -= 1;
-
         SetSoundsVolume(soundsVolume);
     }
 
-    /// <summary>
-    /// Set sounds volume
-    /// </summary>
+
     private void SetSoundsVolume(int soundsVolume)
     {
         float muteDecibels = -80f;
-
         if (soundsVolume == 0)
         {
             GameResources.Instance.soundsMasterMixerGroup.audioMixer.SetFloat("soundsVolume", muteDecibels);

4eea7c6b23c6278a132c5db129e13bb9ecf3dc8f
Mark Yu <zyu347@uwo.ca>
2024-02-26 20:53:22 -0500

    added music

--- Assets/_Scripts/UI/HighScoreManager.cs
+++ Assets/_Scripts/UI/HighScoreManager.cs
@@ -13,9 +13,6 @@ public class HighScoreManager : SingletonMonobehaviour<HighScoreManager>
         LoadScores();
     }
 
-    /// <summary>
-    /// Load Scores From Disk
-    /// </summary>
     private void LoadScores()
     {
         BinaryFormatter bf = new BinaryFormatter();
@@ -33,33 +30,21 @@ public class HighScoreManager : SingletonMonobehaviour<HighScoreManager>
         }
     }
 
-    /// <summary>
-    /// Clear All Scores
-    /// </summary>
     private void ClearScoreList()
     {
         highScores.scoreList.Clear();
     }
 
-    /// <summary>
-    /// Add score to high scores list
-    /// </summary>
     public void AddScore(Score score, int rank)
     {
         highScores.scoreList.Insert(rank - 1, score);
-
-        // Maintain the maximum number of scores to save
         if (highScores.scoreList.Count > Settings.numberOfHighScoresToSave)
         {
             highScores.scoreList.RemoveAt(Settings.numberOfHighScoresToSave);
         }
-
         SaveScores();
     }
 
-    /// <summary>
-    /// Save Scores To Disk
-    /// </summary>
     private void SaveScores()
     {
         BinaryFormatter bf = new BinaryFormatter();
@@ -71,38 +56,26 @@ public class HighScoreManager : SingletonMonobehaviour<HighScoreManager>
         file.Close();
     }
 
-    /// <summary>
-    /// Get highscores
-    /// </summary>
     public HighScores GetHighScores()
     {
         return highScores;
     }
 
-    /// <summary>
-    /// Return the rank of the playerScore compared to the other high scores (returns 0 if the score isn't higher than any in the high scores list)
-    /// </summary>
+
     public int GetRank(long playerScore)
     {
-        // If there are no scores currently in the list - then this score must be ranked 1 - then return
         if (highScores.scoreList.Count == 0) return 1;
-
         int index = 0;
-
-        // Loop through scores in list to find the rank of this score
         for (int i = 0; i < highScores.scoreList.Count; i++)
         {
             index++;
-
             if (playerScore >= highScores.scoreList[i].playerScore)
             {
                 return index;
             }
         }
-
         if (highScores.scoreList.Count < Settings.numberOfHighScoresToSave)
             return (index + 1);
-
         return 0;
     }
 }
